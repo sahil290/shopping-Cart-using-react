91b5dfbad2ca122b62782d1412739491f0ac7534
SAHIL KATOCH <80945456+sahil290@users.noreply.github.com>
2021-09-19 01:01:10 +0530

    Add files via upload

---
+++ src/Cart.js
@@ -0,0 +1,72 @@
+import { useCart } from "react-use-cart";
+function Cart() {
+  const {
+    isEmpty,
+    items,
+    cartTotal,
+    updateItemQuantity,
+    removeItem,
+    emptyCart,
+  } = useCart();
+ 
+  if (isEmpty) {
+    return <h1 className= "text-center mt-4">Your Cart is Empty</h1>;
+  }
+  return (
+    <section className="py-4 container">
+      <div className="row justify-content-center">
+        <div className="col-12">
+          <table className="table table-light table-hover m-0">
+            <tbody>
+              {items.map((item, index) => {
+                return (
+                  <tr key={index}>
+                    <td>
+                      <img src={item.img} style={{ height: "6rem" }} alt="" />
+                    </td>
+                    <td>{item.title}</td>
+                    <td>{item.price}</td>
+                    <td>Quantity ({item.quantity})</td>
+                    <td>
+                      <button
+                        className="btn btn-info ms-2"
+                        onClick={() =>
+                          updateItemQuantity(item.id, item.quantity - 1)
+                        }
+                      >
+                        -
+                      </button>
+                      <button
+                        className="btn btn-info ms-2"
+                        onClick={() =>
+                          updateItemQuantity(item.id, item.quantity + 1)
+                        }
+                      >
+                        +
+                      </button>
+                      <button
+                        className="btn btn-danger ms-2"
+                        onClick={() => removeItem(item.id)}
+                      >
+                        Remove Item
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+        <div className="col-auto ms-auto">
+          <h2>Total Price Rs: {cartTotal}</h2>
+        </div>
+        <div className="col-auto">
+          <button className="btn btn-danger m-2" onClick={() => emptyCart()}>
+            Clear Cart
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+}
+export default Cart;
---
+++ src/data.js
@@ -0,0 +1,32 @@
+import img1 from './Images/Img1.png';
+import img2 from './Images/Img2.png';
+import img3 from './Images/Img3.png';
+const data = {
+    productData: [
+        {
+            id: 1,
+            img: img1,
+            title: "Sleeve Shirt",
+            desc: "This is a brand New Shirt. And Right now you can get it on a big discount.",
+            price: 250,
+            discount:300,
+        },
+        {
+            id: 2,
+            img: img2,
+            title: "Cotton Shirt",
+            desc: "This is a brand New Shirt. And Right now you can get it on a big discount.",
+            price: 550,
+            discount : 600,
+        },
+        {
+            id: 3,
+            img: img3,
+            title: "Chocolate Shirt",
+            desc: "This is a brand New Shirt. And Right now you can get it on a big discount.",
+            price: 200,
+            discount: 250,
+        }
+    ],
+};
+export default data;
